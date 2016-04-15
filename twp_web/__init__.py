10f6d97b9eeb8667dbe52fdacfb0f2ff6e5dee85
Kendrick Cline <kdecline@gmail.com>
2016-04-15 11:03:46 -0400

    added multiple logins and TWP_PATH

--- twp_web/__init__.py
+++ twp_web/__init__.py
@@ -22,27 +22,43 @@ def index():
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    flash('New entry was successfully posted')
-    return redirect(url_for('index'))
+  if not session.get('logged_in'):
+    abort(401)
+  flash('New entry was successfully posted')
+  return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+  error = None
+  logged_in = None
+  if request.method == 'POST':
+    # check username/password
+    for username, password in app.config['ACCOUNTS'].iteritems():
+      if request.form['username'] == username:
+        if request.form['password'] == password:
+          logged_in = True
+          break
         else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
+          logged_in = False
+      else:
+        logged_in = False
+
+  # ensure we return the correct value
+  if logged_in:
+      session['logged_in'] = True
+      flash('You were logged in')
+      return redirect(url_for('index'))
+  elif logged_in is False:
+    error = 'Invalid credentials'
+
+  return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('index'))
+  session.pop('logged_in', None)
+  flash('You were logged out')
+  return redirect(url_for('index'))
+
+@app.before_first_request
+def check_location_on_startup():
+  print 'Asserting that things are where we expect them'
--- twpwebconfig.py
+++ twpwebconfig.py
@@ -1,4 +1,10 @@
+import os
 
-USERNAME = 'kenny'
-PASSWORD = 'test'
-SECRET_KEY = 'asjdkflsd'
+ACCOUNTS = {
+    'robert': 'trenary',
+    'kendrick': 'cline',
+    'scott': 'linder'
+}
+SECRET_KEY = '9A402F81-DDB3-4D40-9027-E2F8E1BC5992'
+
+TWP_HOME = os.path.abspath('../')
