c0b1298e2e5117d724fbe012377638d23678a521
Kendrick Cline <kdecline@gmail.com>
2016-08-13 10:11:17 -0500

    gitolite config parser

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ ACCOUNTS = {
 	'kendrick': 'cline',
 	'scott': 'linder',
 	'chris': 'sphinx',
-	'robert': 'trenary'
+  'robert': 'trenary'
 }
 
 import os
@@ -13,7 +13,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 TWP_HOME = os.path.abspath('..')
 TWP_BAKUPS = os.path.join(TWP_HOME, 'bak')
 TWP_REPOS = os.path.join(TWP_HOME, 'repositories')
+TWP_GITOLITE = os.path.join(TWP_HOME, 'gitolite-admin')
 
 THREADS_PER_PAGE = 2
 
 SECRET_KEY = '9A402F81-DDB3-4D40-9027-E2F8E1BC5992'
+
